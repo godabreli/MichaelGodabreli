b1ab7913a060e58e5947fed81090b58bba63c35e
Godabreli <godabrelidze@hotmail.com>
2025-11-02 12:08:31 +0100

    kleine Probleme behoben

--- src/Navigation/NavigationLinks.jsx
+++ src/Navigation/NavigationLinks.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import "./Navigationlinks.css";
+import "./NavigationLinks.css";
 
 function NavigationLinks(props) {
   return (
